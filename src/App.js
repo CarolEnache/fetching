8a332bf9bf874d4202682db43e45ad0fedaa0774
Carol Enache <enache.carol@gmail.com>
2018-07-17 22:15:51 +0100

    create functionality to search by city name

--- src/App.js
+++ src/App.js
@@ -1,55 +1,62 @@
 import React, { Component } from 'react';
 import './App.css';
 
-const API = 'http://api.openweathermap.org/data/2.5/forecast?id=524901&APPID='
-const KEY = '3de18c1ff4b1ba20a7a46d1a4b524e00';
+const API = 'http://api.openweathermap.org/data/2.5/forecast?q='
+const location = 'Miami';
+const unit = '&units=metric';
+const KEY = '?id=524901&APPID=3de18c1ff4b1ba20a7a46d1a4b524e00';
 
 class App extends Component {
 
-  constructor(props){
+  constructor(props) {
     super(props);
 
     this.state = {
       data: {},
+      city: {},
+      location: '',
+      lists: [],
       isLoading: false,
       error: null,
     };
   }
-
+ 
+  // TODO: find a way to have ony one fetch() function 
   componentDidMount() {
     // this.setState({ isLoading: true });
-
-    fetch(API + KEY)
+    fetch(API + location + unit + KEY)
       .then(response => {
-        if(response.ok) {
+        if (response.ok) {
           return response.json()
         } else {
-        throw new Error('something went wrong...')
+          throw new Error('something went wrong...')
         }
       })
-      // .then(data => console.log(data.city.name))
-      // .then(data => console.log(data))
-      .then(data => this.setState({ data: data}))
-      .catch(error => this.setState({error, isLoading: false}))
+      .then(data => this.setState({ data: data, city: data.city, lists: data.list }))
+      .catch(error => this.setState({ error, isLoading: false }))
   }
 
-  
+  handleSubmit = (event) => {
+    event.preventDefault()
+    fetch(API + this.state.location + unit + KEY)
+      .then(response => {
+        if (response.ok) {
+          return response.json()
+        } else {
+          throw new Error('something went wrong...')
+        }
+      })
+      .then(data => this.setState({ data: data, city: data.city, lists: data.list }))
+      .catch(error => this.setState({ error, isLoading: false }))
+  }
+
+  handleChange = (event) => {
+    this.setState({ location: event.target.value })
+  }
 
   render() {
-
-    const { data, isLoading, error } = this.state;
-    const city = data.city
-    // var something = ''
-    console.log(data.city)
-
-    for (const prop in city) {
-      console.log(`city.${prop}`, 1)
-      console.log(`${city[prop]}`, 2)
-      var something = city[prop];
-    }
-    console.log(something)
-    
-
+    const { data, city, lists, isLoading, error } = this.state;
+    console.log(this.state)
     if (error) {
       return <h1>{error.message}</h1>
     }
@@ -57,10 +64,24 @@ class App extends Component {
     if (isLoading) {
       return <h1>Loading...</h1>
     }
-    
-    // console.log(this.state)
+
     return (
-      <h1>numeber</h1>
+      <div>
+        <h1>{city.name} {city.country} population: {city.population}</h1>
+        <form onSubmit={this.handleSubmit}>
+          <input type='text' onChange={this.handleChange}/>
+          <input type='submit' value='submit'/>
+        </form>
+        <ul>{
+          lists.map(list => {
+            let celsius = list.main.temp - 273;
+            let tempreture = Math.round(celsius)
+            return <li key={list.main.temp}>{tempreture}</li>
+          })
+        }
+        </ul>
+      </div>
+
     );
   }
 }
